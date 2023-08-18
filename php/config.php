3ed1cf14b98c741ea7bb9b67b5828e5168aa516d
Anand Soni <aanandsoni50@gmail.com>
2023-08-18 16:48:34 +0530

    ~

--- php/config.php
+++ php/config.php
@@ -7,7 +7,7 @@
     3. Go to JavaScript file and search this keyword - let domain - then paste your url there 
     4. After all changes you've to wait because javascript file save changes may take time to reflect */ 
 
-    $domain = "localhost/url/"; //like this: codingnepalweb.com/
+    $domain = "localhost/url/"; //like this: prototype47.com/
     $host = "localhost";
     $user = "root"; //Database username
     $pass = ""; //Database password
